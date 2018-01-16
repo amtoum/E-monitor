34b681721caeae463e08a035cb277aa79d24d67b
amtoum <toumia.amri@gmail.com>
2018-01-16 20:55:34 +0100

    petite modif roue

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -165,12 +165,12 @@ class AuthController extends Zend_Controller_Action
     	
     	// For production use set the CA certificate that is the issuer of the cert
     	// on the CAS server and uncomment the line below
-    	phpCAS::setCasServerCACert("cacert.pem");//($cas_server_ca_cert_path);
+    	// phpCAS::setCasServerCACert("cacert.pem");//($cas_server_ca_cert_path);
     	
     	// For quick testing you can disable SSL validation of the CAS server.
     	// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
     	// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-    	// phpCAS::setNoCasServerValidation();
+    	phpCAS::setNoCasServerValidation();
     	
     	// force CAS authentication
     	phpCAS::forceAuthentication();
